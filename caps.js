6f948310cbdec12f3621c8799d86f5f290ea0166
TrunkOfUkuleles <60909020+TrunkOfUkuleles@users.noreply.github.com>
2021-05-03 18:33:25 -0700

    workin

--- caps.js
+++ caps.js
@@ -1,7 +1,25 @@
 'use strict';
 
-const events = require('./event-pool.js');
+const events = require('./events.js');
+let driver = require('./modules/driver.js')
+let start = require('./modules/vendor.js')
 
-events.on('', payload =>{
-    
+
+console.log("CAN I SEE?")
+start();
+driver();
+events.on('ready-for-pickup', payload =>{
+    console.log("EVENT ", payload)
+
+    events.emit('pickup', payload)
+})
+
+events.on('picked-up', payload =>{
+    console.log("EVENT ", payload)
+})
+
+events.on('delivered', payload =>{
+    console.log("EVENT ", payload)
+
+    events.emit('delivery-notice', payload)
 })
