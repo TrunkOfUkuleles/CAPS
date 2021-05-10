471f9741e08505447cbe6b1832299a4a9196cb8b
TrunkOfUkuleles <60909020+TrunkOfUkuleles@users.noreply.github.com>
2021-05-09 23:22:55 -0700

    working locally

--- modules/driver.js
+++ modules/driver.js
@@ -4,17 +4,16 @@
 const io = require('socket.io-client');
 require('dotenv').config();
 const HOST = process.env.HOST || 'http://localhost:3000';
-const EEVENT = require('./Event.js')
 let deliverySystem = io.connect(`${HOST}/caps`);
 
 let safer = (pay, idd) =>{
         console.log("Driver: delivered " + idd)
-        deliverySystem.emit('delivered', EEVENT('delivered', pay))
+        deliverySystem.emit('delivered', pay)
     }
 
 let safe = (pay, idd) =>{
         console.log("Driver: Picked up " + idd)
-        deliverySystem.emit('picked-up', EEVENT('in-transit', pay))
+        deliverySystem.emit('in-transit', pay)
         setTimeout(safer, 3000, pay, idd)
     }
 
